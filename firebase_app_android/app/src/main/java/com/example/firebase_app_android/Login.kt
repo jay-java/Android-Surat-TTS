667c05108cb3cf67c9dc5a8dbf4e6720973188e5
jay-java <jayprajapati.tops@gmail.com>
2024-12-12 10:19:06 +0530

    google sign in

--- firebase_app_android/app/src/main/java/com/example/firebase_app_android/Login.kt
+++ firebase_app_android/app/src/main/java/com/example/firebase_app_android/Login.kt
@@ -5,12 +5,22 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firebase_app_android.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class Login : AppCompatActivity() {
 
+    companion object{
+        private  const val RC_SIGN_IN = 9001
+    }
+
     private lateinit var binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient : GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +33,12 @@ class Login : AppCompatActivity() {
         }
 
         firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.googleSignInBtn.setOnClickListener {
+            signIn()
+        }
+
+
 
         binding.loginBtn.setOnClickListener {
             val email = binding.emailEditeText.text.toString()
@@ -50,5 +66,49 @@ class Login : AppCompatActivity() {
             val intent = Intent(this, home_activity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun signIn(){
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this,gso)
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent,RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)
+                account.idToken?.let { firebaseAuthViaGoogle(it) }
+            }
+            catch (e:ApiException){
+                Toast.makeText(this,"failed : ${e.message}",Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    private fun firebaseAuthViaGoogle(idToken:String){
+        val credential = GoogleAuthProvider.getCredential(idToken,null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener {
+                task ->
+                if(task.isSuccessful){
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(this,"signed in ${user?.displayName}",Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this,home_activity::class.java))
+                    finish()
+                }
+                else{
+                    Toast.makeText(this,"MSG",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- firebase_app_android/app/src/main/java/com/example/firebase_app_android/home_activity.kt
+++ firebase_app_android/app/src/main/java/com/example/firebase_app_android/home_activity.kt
@@ -1,24 +1,63 @@
 package com.example.firebase_app_android
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.firebase_app_android.databinding.ActivityHomeBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class home_activity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,gso)
+
+        val userName = binding.userNameTextId
+
+        val auth = Firebase.auth
+        val user = auth.currentUser
+
+        if(user !=null){
+            val user_name =  user.displayName
+            userName.text = "Welcome, "+user_name
         }
+        else{
+            Toast.makeText(this,"MSG",Toast.LENGTH_SHORT).show()
+        }
+
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+
+            googleSignInClient.signOut().addOnCompleteListener {
+                val intent = Intent(this,Login::class.java)
+                startActivity(intent)
+                finish()
+            }
+
+        }
+
     }
 }
